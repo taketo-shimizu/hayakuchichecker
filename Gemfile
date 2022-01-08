91c3b802388ee39168fba0a25645e677c616b32d
taketo-shimizu <create.taketo@gmail.com>
2022-01-09 01:51:37 +0900

    本番環境にAWSS3を設定

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'font-awesome-sass', '~> 5.12.0'
 gem 'gon'
 gem 'carrierwave-audio'
 gem 'carrierwave'
+gem 'fog-aws'
+gem 'dotenv-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_directory  = 'hayakuchichecker'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: 'AKIASUNZBZSNNU6BZEN4',
+    aws_secret_access_key: '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',
+    region: ENV['AWS_DEFAULT_REGION'],
+    path_style: true
+  }
+
+end
