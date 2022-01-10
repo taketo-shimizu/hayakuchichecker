f50399d380da84db6d81162710a09d09ab519db5
taketo-shimizu <create.taketo@gmail.com>
2022-01-10 22:15:19 +0900

    本番環境にAWSS3を設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,18 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'hayakuchi-checker'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: 'AKIASUNZBZSNNAZCZBMP',
-    aws_secret_access_key: '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',
-    region: 'ap-northeast-1',
-    path_style: true
-  }
-
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'hayakuchi-checker'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIASUNZBZSNNAZCZBMP',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1',
+      path_style: true
+    }
+  else
+    config.storage :file
+  end
 end
