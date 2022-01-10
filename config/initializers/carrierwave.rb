3f3eacf39a4443741b13e0fdfa727b891e19e203
taketo-shimizu <create.taketo@gmail.com>
2022-01-10 19:05:23 +0900

    本番環境にAWSS3を設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'hayakuchichecker'
+  config.fog_directory  = 'hayakuchi-checker'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIASUNZBZSNNU6BZEN4',
-    aws_secret_access_key: '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',
-    region: 'ap-northeast-3',
+    aws_access_key_id: 'AKIASUNZBZSNNAZCZBMP',
+    aws_secret_access_key: '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',
+    region: 'ap-northeast-1',
     path_style: true
   }
 
